ce3d90b95662d7f51602e7c5203841f239c2a5bd
skino0 <skinogigo@gmail.com>
2019-02-12 19:46:00 +0200

    finished

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 
 $('#button').hover(function(){
-        $('#bg').addClass('change');
-        $('.triangle').addClass('anim');
-
+    $('#bg').addClass('change');
+    $('.triangle').addClass('anim');
 });
 
 $('#button').mouseout(function(){
     $('#bg').removeClass('change');
     $('.triangle').removeClass('anim');
+});
 
+$('#button').click(function(){
+    $('.triangle').addClass('test');
+    $('#over').addClass('activate');
+    $(this).addClass('leave');
 });
